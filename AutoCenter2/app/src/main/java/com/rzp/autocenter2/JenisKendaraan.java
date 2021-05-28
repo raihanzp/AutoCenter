97c65ec6cf04c58bf2127cbe28bc898e767c8a8d
RAIHANZP <raihanprayogi48@gmail.com>
2021-05-28 13:50:16 +0700

    FirstCommit

---
+++ AutoCenter2/app/src/main/java/com/rzp/autocenter2/JenisKendaraan.java
@@ -0,0 +1,14 @@
+package com.rzp.autocenter2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JenisKendaraan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jenis_kendaraan);
+    }
+}
